479e2a5f73c16a908f40680ccf4cd5e7d0edc149
peterfchris <peter.f.chris@gmail.com>
2020-12-10 09:28:27 -0700

    clear tasks functionality added

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ function loadEventListeners() {
   form.addEventListener('submit', addTask)
   // Remove task event
   taskList.addEventListener('click', removeTask)
+  // Clear tasks event
+  clearBtn.addEventListener('click', clearTasks)
 }
 
 // Add Task
@@ -53,4 +55,17 @@ function removeTask(e){
       e.target.parentElement.parentElement.remove()
     }
   }
+}
+
+// CLear Tasks
+function clearTasks(){
+  // Slower method
+  // taskList.innerHTML = ''
+
+  // Faster method
+  while(taskList.firstChild) {
+    taskList.removeChild(taskList.firstChild)
+  }
+
+  // https://jsperf.com/innerhtml-vs-removechild
 }
